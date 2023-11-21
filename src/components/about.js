d422dc13af0ea4215286233f4fe5d1ba10f24126
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-21 15:41:43 +0300

    Added and used the react query method and it worked gracefully

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
 
 const About = () => {
+const {data, isError,isLoading,refetch}= useQuery(
+    {
+        queryKey: ["cat"],
+        queryFn:()=>axios.get('https://www.boredapi.com/api/activity').then((res)=> res.data)
+    }
+)
+if (isError) {
+    return <h1>Sorry error</h1>
+}
+if (isLoading) {
+    return <h1>Loading...</h1>
+}
+
+
+
   return (
-    <div>About</div>
+    <div>
+        <p>Activity facts: <p>{data?.activity}</p>
+        <p>{data?.type}</p>
+        <p>{data?.participants}</p>
+        </p>
+        <button onClick={refetch}>Update Fact</button>
+    </div>
   )
 }
 
