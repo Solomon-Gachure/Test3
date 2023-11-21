3c3bdfafd26dddc3563d5b9a3f0daf0d81f4985b
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-21 15:25:18 +0300

    Used the react query method and it worked without a hitch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import Home from './components/home';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 function App() { 
-  const client= new QueryClient();
+  const client= new QueryClient({defaultOptions:{
+    queries:{
+      refetchOnWindowFocus:false
+    }
+  }});
   return (
     <div className="App">
       <QueryClientProvider client={client}>
--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
 
 const Home = () => {
+    const {data, isLoading, isError, refetch} =useQuery(
+        {
+            queryKey: ["cat"],
+         queryFn:()=>axios.get('https://catfact.ninja/fact').then((res)=>res.data),
+        }
+    );
+    if (isError) {
+        return <h1>Sorry,caught an error</h1>
+    }
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
   return (
-    <div>Home</div>
+    <div>
+        <p>{data?.fact}</p>
+        <button onClick={refetch}>Update Facts</button>
+    </div>
   )
 }
 
