dd8613c84a1face334b6eb60ed05c1f47ca45560
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-21 14:39:18 +0300

    Created the home page to test out the react query method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
-
-function App() {
+import Home from './components/home';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+function App() { 
+  const client= new QueryClient();
   return (
     <div className="App">
+      <QueryClientProvider client={client}>
+        <Home/>
+      </QueryClientProvider>
       
     </div>
   );
