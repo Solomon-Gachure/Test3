9abfa4a1ce0b12361f78644c31d1188b2e8e284a
Solomon Gachure <gachuresolomon64@gmail.com>
2023-11-22 09:53:48 +0300

    Used the react query function and axios as well to fetch the API data and it worked perfectly

--- src/components/jokes.js
+++ src/components/jokes.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import {useQuery} from '@tanstack/react-query'
+import axios from 'axios'
 
 const Jokes = () => {
+const {data, isLoading,isError,refetch}=useQuery(
+    {
+        queryKey:["jokes"],
+        queryFn:()=>axios.get('https://official-joke-api.appspot.com/random_joke').then((res)=>res.data)
+    }
+)
+if (isLoading) {
+    return <h1>Loading...</h1>
+}
+if (isError) {
+    return <h1>Caught an error</h1>
+}
+
+
   return (
-    <div>Jokes</div>
+    <div>
+        <p>Get jokes:
+            <p>{data?.setup}</p>
+            <p>{data?.punchline}</p>
+
+        </p>
+        <button onClick={refetch}>Update Fact</button>
+    </div>
   )
 }
 
